bf8e54ba9416d189213e3b635f86a9eee92315d1
EanNewton <eannewton5@gmail.com>
2020-09-09 06:02:38 -0700

    Significant update

--- tfilter.py
+++ tfilter.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
 import os
-from sqlalchemy import create_engine, update, insert, select, and_
+from sqlalchemy import update, insert, select, and_
 from sqlalchemy import MetaData, Table, Column, Integer, String 
 
-from tutil import debug, fetchFile, fetch_value, guildList
+from tutil import debug, fetchFile, fetch_value, guildList, is_admin, incrementUsage
+from constants import DEFAULT_DIR, ENGINE
 
-DEFAULT_DIR = os.path.dirname(os.path.abspath(__file__))
 blacklistLow = set()
 blacklistStrict = set()
 blacklistCustom = dict()
 
 
 def setup():
-	global engine, meta, Filters
-	engine = create_engine('sqlite:///./log/quotes.db', echo = False)
+	global meta, Filters
 	meta = MetaData()
 	Filters = Table(
 		'filters', meta,
@@ -22,42 +21,42 @@ def setup():
 		Column('level', Integer),
 		Column('guild_id', String),
 		)
-	meta.create_all(engine)
+	meta.create_all(ENGINE)
 	importBlacklists()
 	print('[+] End filters Setup')
 	
-		
+#TODO add custom
 def helper(message):
-	args = message.content.split()
-	operator = args[1].lower()
-	return {
-		'get': lambda: getFilter(message),
-		'set': lambda: insertFilter(message, args[2]),
-		'clear': lambda: insertFilter(message, 0),
-		'help': lambda: getHelp(),
-	}.get(operator, lambda: None)()
+	incrementUsage(message.guild, 'filter')
+	if is_admin(message.author):
+		args = message.content.split()
+		operator = args[1].lower() if len(args) > 1 else 'help'
+		return {
+			'get': lambda: getFilter(message),
+			'set': lambda: insertFilter(message, args[2]),
+			'clear': lambda: insertFilter(message, 0),
+			'help': lambda: getHelp(),
+		}.get(operator, lambda: None)()
 
 
 def check(message):
-	try:
-		text = message.content.split()
-		fLevel = getFilter(message)
-		custom = blacklistCustom[str(message.guild.id)]
-		lists = {'2': (blacklistStrict), '1': (blacklistLow), '3': (custom)}
-		if fLevel in lists:
-			for word in text:
-				if word in lists[fLevel]:
-					return True 
-		return False 
-	finally:
-		return False
+	text = message.content.split()
+	fLevel = getFilter(message)
+	custom = blacklistCustom[str(message.guild.id)]
+	lists = {'2': (blacklistStrict), '1': (blacklistLow), '3': (custom)}
+		
+	if fLevel in lists:
+		for word in text:
+			if word in lists[fLevel]:
+				return True 
+	return False 
 
 
 def importBlacklists():
 	listOfFiles_low = list()
 	listOfFiles_high = list()
 	
-	for (dirpath, dirnames, filenames) in os.walk(DEFAULT_DIR+'/docs/blacklists'):
+	for (dirpath, _, filenames) in os.walk(DEFAULT_DIR+'/docs/blacklists'):
 		if 'low' in dirpath:
 			listOfFiles_low += [os.path.join(dirpath, file_) for file_ in filenames]
 		elif 'strict' in dirpath:
@@ -88,7 +87,7 @@ def importCustom(guild):
 
 
 def insertFilter(message, level):
-	with engine.connect() as conn:
+	with ENGINE.connect() as conn:
 		if getFilter(message):
 			ins = Filters.update().where(and_(
 				Filters.c.id == message.channel.id,
@@ -101,11 +100,13 @@ def insertFilter(message, level):
 				guild_id = str(message.guild.id),
 				)
 		conn.execute(ins)
+		importBlacklists()
+		importCustom(message.guild.id)
 	return "Set filter for current channel to {}".format(level)
 
 
 def getFilter(message):
-	with engine.connect() as conn:
+	with ENGINE.connect() as conn:
 		select_st = select([Filters]).where(and_(
 			Filters.c.id == message.channel.id,
 			Filters.c.guild_id == message.guild.id
