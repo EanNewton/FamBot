6be7f24bac351e65425cd638f783cbce98ca19d5
EanNewton <eannewton5@gmail.com>
2020-10-04 04:48:33 -0700

    Support for commands within custom commands
    You can now embed commands within custom commands such as `!custom <QUOTE>` or `!custom2 <!custom> <DATE>`

--- tcustom.py
+++ tcustom.py
@@ -2,13 +2,21 @@
 #TODO add embeds
 #TODO add rich support such as dates
 
+from copy import deepcopy
+
+import pendulum
 from sqlalchemy import and_, update, insert, delete, select, MetaData, Table, Column, Integer, String
 
+import tquote
+import tword
+import tsched
 from tutil import is_admin, debug, guildList, fetch_value
-from constants import ENGINE
+from constants import ENGINE, TZ_ABBRS
 
 customCommands = dict()
 
+def get_userByID(user_id):
+	return main.bot.get_user(user_id)
 
 def setup():
     global meta, Commands
@@ -47,11 +55,56 @@ def importCustomCommands(guild):
 
 
 def get_command(message):
-    """Return a custom command to be sent to Discord"""
+    """
+    Return a custom command to be sent to Discord.
+    :param message: <Discord.message> Raw message object
+    :return: <str> Banner
+    """
     banner = None
+    args = message.content.split()
+    config = tsched.load_config(message.guild.id)
+
+    if args[0] in {'!custom',}:
+        if args[1] == 'help':
+            return getHelp(message)
+        return customCommands[message.guild.id]
+	
+    if config:
+        server_locale = config[2]
+        url = config[6]
+        timestamp = pendulum.now(tz=server_locale).to_datetime_string()
+    else:
+        url = ' '
+        timestamp = pendulum.now(tz='America/New_York').to_datetime_string()
+
     try:
         guildCommands = customCommands[message.guild.id]
-        banner = guildCommands[message.content]
+        banner = guildCommands[args[0]]
+        #for nested commands
+        prev = deepcopy(banner)
+        count = 0
+        while True:
+            if len(banner) + len(prev) > 1990 or count > 50:
+                return banner
+            else:
+                for key in guildCommands:
+                    banner = banner.replace('<{}>'.format(key), guildCommands[key])        
+                #TODO user mentions for <QUOTE>
+                banner = banner.replace('<URL>', url)
+                banner = banner.replace('<NOW>', '{} in {}'.format(timestamp, server_locale))
+                banner = banner.replace('<TIME>', '{} in {}'.format(timestamp, server_locale))
+                banner = banner.replace('<LOCALE>', server_locale)
+                banner = banner.replace('<LOCATION>', server_locale)
+                banner = banner.replace('<AUTHOR>', message.author.name)
+                banner = banner.replace('<GUILD>', message.guild.name)
+                banner = banner.replace('<SCHED>', tsched.getSchedule(message))
+                banner = banner.replace('<QUOTE>', tquote.getQuote(message.guild.id, tquote.Quotes))
+                banner = banner.replace('<LORE>', tquote.getQuote(message.guild.id, tquote.Lore))
+                if banner == prev:
+                    return banner
+                else:
+                    count += 1
+                    prev = deepcopy(banner)
     except:
         pass
     finally:
@@ -59,9 +112,14 @@ def get_command(message):
 
 
 def insert_command(message):
-    """Add a new custom command to the database"""
+    """
+    Add a new custom command to the database.
+    :param message: <Discord.message> Raw message object
+    :return: <str> Banner notifying if new command has been created or exisisting has been updated.
+    """
+
     if is_admin(message.author):
-        args = message.content.split()	
+        args = message.content.split()
 
         with ENGINE.connect() as conn:
             select_st = select([Commands]).where(and_(
@@ -110,5 +168,11 @@ def delete_command(message):
                 conn.execute(del_st)
                 importCustomCommands(message.guild.id)
                 return 'Deleted {}'.format(args[0])
+
+
+def getHelp(message):
+    pass
+
+
 
 setup()
